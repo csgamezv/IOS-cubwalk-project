f1058d481a1dd9af33ddd55f3e618f95aabf233e
Cesar Gamez <cesargamez001@berkeley.edu>
2022-04-28 16:56:59 -0700

    Added Xcode project

---
+++ CubWalk/CubWalk/CubWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubWalkApp.swift
+//  CubWalk
+//
+//  Created by Cesar Gamez on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CubWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
