1bd7e742777d6c08869379d1c4271f026774bd4c
Cesar Gamez <cesargamez001@berkeley.edu>
2022-04-30 15:35:13 -0700

    added map stuff

--- CubWalk/CubWalk/Views/MapView.swift
+++ CubWalk/CubWalk/Views/MapView.swift
@@ -10,16 +10,17 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 37.334_900,
-                                           longitude: -122.009_020),
-            latitudinalMeters: 750,
-            longitudinalMeters: 750
-        )
-        
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.8715, longitude: -122.2620), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+
         var body: some View {
+            VStack{
+                TopMenuView().edgesIgnoringSafeArea(.top).padding(.top,5)
             Map(coordinateRegion: $region)
+            Spacer()
+        
+            }
         }
+            
 }
 
 struct MapView_Previews: PreviewProvider {
